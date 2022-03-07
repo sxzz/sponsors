31d82f26b19efa694b1a5775b4e805c32dcd38b1
三咲智子 <sxzz@sxzz.moe>
2022-03-07 16:04:43 +0800

    feat: init project

---
+++ sponsorkit.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+  tiers: [
+    {
+      title: 'Backers',
+      // to replace the entire tier rendering
+      // compose: (composer, tierSponsors, config) => {
+      //   composer.addRaw(
+      //     '<-- custom svg -->',
+      //   )
+      // },
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 5,
+      preset: presets.medium,
+      // to insert custom elements after the tier block
+      composeAfter: (composer, tierSponsors, config) => {
+        composer.addSpan(10)
+      },
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 20,
+      preset: presets.large,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 100,
+      preset: presets.xl,
+    },
+  ],
+})
